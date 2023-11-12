38a3cd81bb160b14888109cfd8bd965ed3a7c781
David Livengood <dlivengood@worcester.edu>
2023-11-12 15:43:27 -0500

    Improved game initialization comments

--- src/gameInitialization.ts
+++ src/gameInitialization.ts
@@ -13,7 +13,11 @@ class GameInitializationError extends Error {
 }
 
 /**
- * The class that handles the initialization of the game components
+ * Initializes the game components.
+ * @param {CanvasInitialization} canvasInitialization - The canvas initialization instance.
+ * @param {EntityManager} entityManager - The entity manager instance.
+ * @param {SystemManager} systemManager - The system manager instance.
+ * @throws {GameInitializationError} Throws an error if initialization fails.
  */
 export function initializeGameComponents(
   canvasInitialization: CanvasInitialization,
@@ -36,6 +40,8 @@ export function initializeGameComponents(
 
 /**
  * Handles errors during initialization.
+ * @param {Error} error - The error that occurred during initialization.
+ * @throws {GameInitializationError} Throws a game initialization error.
  */
 function handleInitializationError(error: Error): void {
   console.error('Error during game initialization:', error);

c0e07c05c5e97c9240a8a4ce6d949236441e929a
David Livengood <dlivengood@worcester.edu>
2023-11-12 15:49:14 -0500

    added comments to canvasInitialization

--- src/canvasInitialization.ts
+++ src/canvasInitialization.ts
@@ -1,9 +1,16 @@
 import { CanvasValues } from './constants';
 
+/**
+ * Class representing the initialization of a canvas for rendering graphics.
+ */
 export class CanvasInitialization {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D | null;
 
+  /**
+   * Constructs a new CanvasInitialization instance.
+   * @throws {Error} Throws an error if the CanvasRenderingContext2D cannot be initialized.
+   */
   constructor() {
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
@@ -15,15 +22,28 @@ export class CanvasInitialization {
     this.configureCanvas();
   }
 
+  /**
+   * Configures the canvas with predefined width and height.
+   * @private
+   */
   private configureCanvas() {
     this.canvas.width = CanvasValues.WIDTH;
     this.canvas.height = CanvasValues.HEIGHT;
   }
 
+  /**
+   * Gets the HTMLCanvasElement instance.
+   * @returns {HTMLCanvasElement} The canvas element.
+   */
   getCanvas(): HTMLCanvasElement {
     return this.canvas;
   }
 
+  /**
+   * Gets the CanvasRenderingContext2D instance.
+   * @throws {Error} Throws an error if the CanvasRenderingContext2D is null.
+   * @returns {CanvasRenderingContext2D} The 2D rendering context.
+   */
   getContext(): CanvasRenderingContext2D {
     if (this.ctx === null) {
       throw new Error('CanvasRenderingContext2D is null.');

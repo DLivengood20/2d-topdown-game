633593fd68f3fb17bd8ac41ad743aec00651172f
David Livengood <dlivengood@worcester.edu>
2023-11-12 15:38:45 -0500

    Added comments to system and component interfaces

--- src/systems/system.ts
+++ src/systems/system.ts
@@ -1,5 +1,14 @@
 import { Entity } from '../entities/entity';
 
+/**
+ * Interface representing a game system.
+ * Game systems are responsible for updating entities in the game.
+ * @interface
+ */
 export interface System {
+  /**
+   * Updates the game system with the provided entities.
+   * @param {Entity[]} entities - The entities to be updated.
+   */
   update(entities: Entity[]): void;
 }

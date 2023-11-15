3cb0fd8c83d893dc6ce6b139a3b2a65c621b8818
David Livengood <dlivengood@worcester.edu>
2023-11-15 18:47:08 -0500

    Simplified methods into smaller easier to understand parts

--- src/systems/input.system.ts
+++ src/systems/input.system.ts
@@ -18,96 +18,117 @@ import { WeaponComponent } from '../components/weapon.component';
 export class InputSystem implements System {
   keysPressed: { [key: string]: boolean } = {};
   constructor() {
-    document.addEventListener('keydown', (event) => {
-      this.keysPressed[event.key] = true;
-    });
-
-    document.addEventListener('keyup', (event) => {
-      this.keysPressed[event.key] = false;
-    });
-
-    document.addEventListener('mousedown', (event) => {
-      if (event.button === 0) {
-        this.keysPressed['mousedown'] = true;
-      }
-    });
-
-    document.addEventListener('mouseup', (event) => {
-      if (event.button === 0) {
-        this.keysPressed['mousedown'] = false;
-      }
-    });
+    this.addEventListeners();
   }
 
-  applyInputs(player: PlayerEntity) {
+  private addEventListeners(): void {
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    document.addEventListener('keyup', this.handleKeyUp.bind(this));
+    document.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    document.addEventListener('mouseup', this.handleMouseUp.bind(this));
+  }
+
+  private handleKeyDown(event: KeyboardEvent): void {
+    this.keysPressed[event.key] = true;
+  }
+
+  private handleKeyUp(event: KeyboardEvent): void {
+    this.keysPressed[event.key] = false;
+  }
+
+  private handleMouseDown(event: MouseEvent): void {
+    if (event.button === 0) {
+      this.keysPressed['mousedown'] = true;
+    }
+  }
+
+  private handleMouseUp(event: MouseEvent): void {
+    if (event.button === 0) {
+      this.keysPressed['mousedown'] = false;
+    }
+  }
+
+  private applyInputs(player: PlayerEntity): void {
     const statusComponent =
       player.getComponent<StatusComponent>(StatusComponent);
-
-    const physicalComponent =
-      player.getComponent<PhysicalComponent>(PhysicalComponent);
-
-    if (
-      statusComponent &&
-      (statusComponent.isStunned || statusComponent.isAttacking)
-    ) {
+    if (!statusComponent || this.shouldSkipInput(statusComponent)) {
       return;
     }
 
-    if (physicalComponent && statusComponent?.collidedWith.length === 0) {
-      if (this.keysPressed['ArrowUp'] && this.keysPressed['ArrowLeft']) {
-        moveUpLeft(physicalComponent, physicalComponent.diagonalSpeed, true);
-      } else if (
-        this.keysPressed['ArrowUp'] &&
-        this.keysPressed['ArrowRight']
-      ) {
-        moveUpRight(physicalComponent, physicalComponent.diagonalSpeed, true);
-      } else if (
-        this.keysPressed['ArrowDown'] &&
-        this.keysPressed['ArrowLeft']
-      ) {
-        moveBottomLeft(
-          physicalComponent,
-          physicalComponent.diagonalSpeed,
-          true
-        );
-      } else if (
-        this.keysPressed['ArrowDown'] &&
-        this.keysPressed['ArrowRight']
-      ) {
-        moveBottomRight(
-          physicalComponent,
-          physicalComponent.diagonalSpeed,
-          true
-        );
-      } else if (this.keysPressed['ArrowUp']) {
-        moveUp(physicalComponent, physicalComponent.speed, true);
-      } else if (this.keysPressed['ArrowDown']) {
-        moveDown(physicalComponent, physicalComponent.speed, true);
-      } else if (this.keysPressed['ArrowRight']) {
-        moveRight(physicalComponent, physicalComponent.speed, true);
-      } else if (this.keysPressed['ArrowLeft']) {
-        moveLeft(physicalComponent, physicalComponent.speed, true);
-      }
+    const physicalComponent =
+      player.getComponent<PhysicalComponent>(PhysicalComponent);
+    const weaponComponent =
+      player.getComponent<WeaponComponent>(WeaponComponent);
+
+    if (physicalComponent && this.shouldApplyMovement(statusComponent)) {
+      this.applyMovement(physicalComponent);
     }
 
-    if (this.keysPressed['mousedown']) {
-      const weaponComponent =
-        player.getComponent<WeaponComponent>(WeaponComponent);
-      const statusComponent =
-        player.getComponent<StatusComponent>(StatusComponent);
-      if (
-        weaponComponent &&
-        statusComponent &&
-        Date.now() - statusComponent.attackTimer >= weaponComponent.cooldown
-      ) {
-        statusComponent.isAttacking = true;
-        statusComponent.attackTimer = Date.now();
-        setTimeout(
-          () => (statusComponent.isAttacking = false),
-          weaponComponent.attackDuration
-        );
-      }
+    if (
+      weaponComponent &&
+      this.shouldAttack(weaponComponent, statusComponent)
+    ) {
+      this.attack(weaponComponent, statusComponent);
     }
+  }
+
+  private shouldSkipInput(statusComponent: StatusComponent): boolean {
+    return statusComponent.isStunned || statusComponent.isAttacking;
+  }
+
+  private shouldApplyMovement(statusComponent: StatusComponent): boolean {
+    return (
+      statusComponent.collidedWith.length === 0 &&
+      (this.keysPressed['ArrowUp'] ||
+        this.keysPressed['ArrowDown'] ||
+        this.keysPressed['ArrowRight'] ||
+        this.keysPressed['ArrowLeft'])
+    );
+  }
+
+  private applyMovement(physicalComponent: PhysicalComponent): void {
+    if (this.keysPressed['ArrowUp'] && this.keysPressed['ArrowLeft']) {
+      moveUpLeft(physicalComponent, physicalComponent.diagonalSpeed, true);
+    } else if (this.keysPressed['ArrowUp'] && this.keysPressed['ArrowRight']) {
+      moveUpRight(physicalComponent, physicalComponent.diagonalSpeed, true);
+    } else if (this.keysPressed['ArrowDown'] && this.keysPressed['ArrowLeft']) {
+      moveBottomLeft(physicalComponent, physicalComponent.diagonalSpeed, true);
+    } else if (
+      this.keysPressed['ArrowDown'] &&
+      this.keysPressed['ArrowRight']
+    ) {
+      moveBottomRight(physicalComponent, physicalComponent.diagonalSpeed, true);
+    } else if (this.keysPressed['ArrowUp']) {
+      moveUp(physicalComponent, physicalComponent.speed, true);
+    } else if (this.keysPressed['ArrowDown']) {
+      moveDown(physicalComponent, physicalComponent.speed, true);
+    } else if (this.keysPressed['ArrowRight']) {
+      moveRight(physicalComponent, physicalComponent.speed, true);
+    } else if (this.keysPressed['ArrowLeft']) {
+      moveLeft(physicalComponent, physicalComponent.speed, true);
+    }
+  }
+
+  private shouldAttack(
+    weaponComponent: WeaponComponent,
+    statusComponent: StatusComponent
+  ): boolean {
+    return (
+      this.keysPressed['mousedown'] &&
+      Date.now() - statusComponent.attackTimer >= weaponComponent.cooldown
+    );
+  }
+
+  private attack(
+    weaponComponent: WeaponComponent,
+    statusComponent: StatusComponent
+  ): void {
+    statusComponent.isAttacking = true;
+    statusComponent.attackTimer = Date.now();
+    setTimeout(
+      () => (statusComponent.isAttacking = false),
+      weaponComponent.attackDuration
+    );
   }
 
   update(entities: Entity[]): void {

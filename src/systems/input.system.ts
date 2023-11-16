827891c46c9e9f133797c5be095fde4d4fa5c20f
David Livengood <dlivengood@worcester.edu>
2023-11-15 19:06:36 -0500

    added comments to inputSystem

--- src/systems/input.system.ts
+++ src/systems/input.system.ts
@@ -15,12 +15,26 @@ import { StatusComponent } from '../components/status.component';
 import { System } from './system';
 import { WeaponComponent } from '../components/weapon.component';
 
+/**
+ * System handling user input for player entities.
+ *
+ * @implements {System}
+ */
 export class InputSystem implements System {
+  /** Stores the keys currently pressed. */
   keysPressed: { [key: string]: boolean } = {};
+
+  /**
+   * Constructs the InputSystem and sets up event listeners for keyboard and mouse input.
+   */
   constructor() {
     this.addEventListeners();
   }
 
+  /**
+   * Adds event listeners for keyboard and mouse input.
+   * @private
+   */
   private addEventListeners(): void {
     document.addEventListener('keydown', this.handleKeyDown.bind(this));
     document.addEventListener('keyup', this.handleKeyUp.bind(this));
@@ -28,26 +42,51 @@ export class InputSystem implements System {
     document.addEventListener('mouseup', this.handleMouseUp.bind(this));
   }
 
+  /**
+   * Handles the keydown event and updates the keysPressed object.
+   * @param {KeyboardEvent} event - The keydown event.
+   * @private
+   */
   private handleKeyDown(event: KeyboardEvent): void {
     this.keysPressed[event.key] = true;
   }
 
+  /**
+   * Handles the keyup event and updates the keysPressed object.
+   * @param {KeyboardEvent} event - The keyup event.
+   * @private
+   */
   private handleKeyUp(event: KeyboardEvent): void {
     this.keysPressed[event.key] = false;
   }
 
+  /**
+   * Handles the mousedown event and sets the mousedown flag to true.
+   * @param {MouseEvent} event - The mousedown event.
+   * @private
+   */
   private handleMouseDown(event: MouseEvent): void {
     if (event.button === 0) {
       this.keysPressed['mousedown'] = true;
     }
   }
 
+  /**
+   * Handles the mouseup event and sets the mousedown flag to false.
+   * @param {MouseEvent} event - The mouseup event.
+   * @private
+   */
   private handleMouseUp(event: MouseEvent): void {
     if (event.button === 0) {
       this.keysPressed['mousedown'] = false;
     }
   }
 
+  /**
+   * Applies user inputs to the player entity.
+   * @param {PlayerEntity} player - The player entity.
+   * @private
+   */
   private applyInputs(player: PlayerEntity): void {
     const statusComponent = player.getComponent(StatusComponent);
     if (!statusComponent || this.shouldSkipInput(statusComponent)) {
@@ -69,10 +108,22 @@ export class InputSystem implements System {
     }
   }
 
+  /**
+   * Checks if user input should be skipped based on the status component.
+   * @param {StatusComponent} statusComponent - The status component.
+   * @returns {boolean} True if input should be skipped, otherwise false.
+   * @private
+   */
   private shouldSkipInput(statusComponent: StatusComponent): boolean {
     return statusComponent.isStunned || statusComponent.isAttacking;
   }
 
+  /**
+   * Checks if movement should be applied based on the status component and keys pressed.
+   * @param {StatusComponent} statusComponent - The status component.
+   * @returns {boolean} True if movement should be applied, otherwise false.
+   * @private
+   */
   private shouldApplyMovement(statusComponent: StatusComponent): boolean {
     return (
       statusComponent.collidedWith.length === 0 &&
@@ -83,6 +134,11 @@ export class InputSystem implements System {
     );
   }
 
+  /**
+   * Applies movement to the physical component based on the keys pressed.
+   * @param {PhysicalComponent} physicalComponent - The physical component.
+   * @private
+   */
   private applyMovement(physicalComponent: PhysicalComponent): void {
     if (this.keysPressed['ArrowUp'] && this.keysPressed['ArrowLeft']) {
       moveUpLeft(physicalComponent, physicalComponent.diagonalSpeed, true);
@@ -106,6 +162,13 @@ export class InputSystem implements System {
     }
   }
 
+  /**
+   * Checks if the player should attack based on the keys pressed and weapon cooldown.
+   * @param {WeaponComponent} weaponComponent - The weapon component.
+   * @param {StatusComponent} statusComponent - The status component.
+   * @returns {boolean} True if the player should attack, otherwise false.
+   * @private
+   */
   private shouldAttack(
     weaponComponent: WeaponComponent,
     statusComponent: StatusComponent
@@ -116,6 +179,12 @@ export class InputSystem implements System {
     );
   }
 
+  /**
+   * Initiates an attack by setting the attack flag and resetting the attack timer.
+   * @param {WeaponComponent} weaponComponent - The weapon component.
+   * @param {StatusComponent} statusComponent - The status component.
+   * @private
+   */
   private attack(
     weaponComponent: WeaponComponent,
     statusComponent: StatusComponent
@@ -128,6 +197,10 @@ export class InputSystem implements System {
     );
   }
 
+  /**
+   * Updates the input system by applying inputs to the player entity.
+   * @param {Entity[]} entities - The array of entities.
+   */
   update(entities: Entity[]): void {
     for (const entity of entities) {
       if (entity.id === 'PLAYER') {

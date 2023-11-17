49412f5b90b1f281b0a376eb57aabf2e108a7842
David Livengood <dlivengood@worcester.edu>
2023-11-17 13:00:26 -0500

    EntityManager now store entities in a Set

--- src/entityManager.ts
+++ src/entityManager.ts
@@ -8,11 +8,12 @@ import { RemoveEntityComponent } from './components/removeEntity.component';
  * @class
  */
 export class EntityManager {
-  private entities: Entity[] = [];
+  private entities: Set<Entity> = new Set<Entity>();
 
   /**
    * Initializes default entities.
    * @method
+   * @returns {void}
    */
   initiateEntities(): void {
     const { DEFAULT_PLAYER, ENEMY_1, ENEMY_2, ENEMY_3 } = Characters;
@@ -26,10 +27,10 @@ export class EntityManager {
    */
   createDefaultPlayer(): PlayerEntity {
     const player = Characters.DEFAULT_PLAYER;
-    if (!this.entities.includes(player)) {
-      this.entities.push(player);
+    if (!this.entities.has(player)) {
+      this.entities.add(player);
     } else {
-      console.error('Player already in array');
+      console.error('Player already exists.');
     }
     return player;
   }
@@ -38,40 +39,45 @@ export class EntityManager {
    * Adds a single entity to the manager.
    * @method
    * @param {Entity} entity - The entity to add.
+   * @returns {void}
    */
   addEntity(entity: Entity): void {
-    this.entities.push(entity);
+    this.entities.add(entity);
   }
 
   /**
    * Adds multiple entities to the manager.
    * @method
    * @param {Entity[]} entities - The entities to add.
+   * @returns {void}
    */
   addEntities(entities: Entity[]): void {
-    this.entities.push(...entities);
+    for (const entity of entities) {
+      this.entities.add(entity);
+    }
   }
 
   /**
    * Removes a specific entity from the manager.
    * @method
    * @param {Entity} entity - The entity to remove.
+   * @returns {void}
    */
   removeEntity(entity: Entity): void {
-    const index = this.entities.indexOf(entity);
-    if (index !== -1) {
-      this.entities.splice(index, 1);
-    }
+    this.entities.delete(entity);
   }
 
   /**
    * Removes entities marked for removal based on the presence of `RemoveEntityComponent`.
    * @method
+   * @returns {void}
    */
   removeMarkedEntities(): void {
-    this.entities = this.entities.filter((entity) => {
-      return entity.getComponent(RemoveEntityComponent) === undefined;
-    });
+    for (const entity of this.entities) {
+      if (entity.getComponent(RemoveEntityComponent)) {
+        this.entities.delete(entity);
+      }
+    }
   }
 
   /**
@@ -80,6 +86,6 @@ export class EntityManager {
    * @returns {Entity[]} An array containing all managed entities.
    */
   getAllEntities(): Entity[] {
-    return this.entities;
+    return [...this.entities];
   }
 }

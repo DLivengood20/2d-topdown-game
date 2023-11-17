d547d83827383fca01d1eb40b208b9031c421091
David Livengood <dlivengood@worcester.edu>
2023-11-17 16:57:57 -0500

    commented render component

--- src/components/render.component.ts
+++ src/components/render.component.ts
@@ -1,10 +1,40 @@
 import { Component } from './component';
 
+/**
+ * Represents the rendering properties of an entity in the game.
+ * @implements {Component}
+ */
 export class RenderComponent implements Component {
+  /**
+   * The default color used for rendering the entity.
+   * @type {string}
+   */
   defaultColor: string;
+
+  /**
+   * The current color used for rendering the entity.
+   * @type {string}
+   */
   currentColor: string;
+
+  /**
+   * The color used for rendering the entity's face.
+   * @type {string}
+   */
   facingColor: string;
+
+  /**
+   * The color used for rendering the entity when it is stunned.
+   * @type {string}
+   */
   stunColor: string;
+
+  /**
+   * Creates an instance of RenderComponent.
+   * @param {string} defaultColor - The default color used for rendering the entity.
+   * @param {string} facingColor - The color used for rendering the entity's face'.
+   * @param {string} stunColor - The color used for rendering the entity when it is stunned.
+   */
   constructor(defaultColor: string, facingColor: string, stunColor: string) {
     this.defaultColor = defaultColor;
     this.facingColor = facingColor;

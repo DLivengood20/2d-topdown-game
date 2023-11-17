349b54cc3f8642335c0a844cb34167fb59aa414e
David Livengood <dlivengood@worcester.edu>
2023-11-17 16:54:34 -0500

    commented RemoveEntity component

--- src/components/removeEntity.component.ts
+++ src/components/removeEntity.component.ts
@@ -1,3 +1,7 @@
 import { Component } from './component';
 
+/**
+ * Represents a component indicating that an entity should be removed from the game.
+ * @implements {Component}
+ */
 export class RemoveEntityComponent implements Component {}

e7efdf45e6d89605157a35433aef7e4c84b8375c
David Livengood <dlivengood@worcester.edu>
2023-11-17 17:05:05 -0500

    commented weapon component

--- src/components/weapon.component.ts
+++ src/components/weapon.component.ts
@@ -1,13 +1,55 @@
 import { Component } from './component';
 
+/**
+ * Represents the weapon properties of an entity in the game.
+ * @implements {Component}
+ */
 export class WeaponComponent implements Component {
+  /**
+   * The length of the weapon.
+   * @type {number}
+   */
   length: number;
+
+  /**
+   * The width of the weapon.
+   * @type {number}
+   */
   width: number;
+
+  /**
+   * The swing angle of the weapon in radians.
+   * @type {number}
+   */
   swingAngle: number;
+
+  /**
+   * The duration of the attack action in milliseconds.
+   * @type {number}
+   */
   attackDuration: number;
+
+  /**
+   * The cooldown time between consecutive attacks in milliseconds.
+   * @type {number}
+   */
   cooldown: number;
+
+  /**
+   * The color of the weapon.
+   * @type {string}
+   */
   color: string;
 
+  /**
+   * Creates an instance of WeaponComponent.
+   * @param {number} length - The length of the weapon.
+   * @param {number} width - The width of the weapon.
+   * @param {number} swingAngle - The swing angle of the weapon in radians.
+   * @param {number} attackDuration - The duration of the attack action in milliseconds.
+   * @param {number} cooldown - The cooldown time between consecutive attacks in milliseconds.
+   * @param {string} color - The color of the weapon.
+   */
   constructor(
     length: number,
     width: number,
@@ -19,8 +61,8 @@ export class WeaponComponent implements Component {
     this.length = length;
     this.width = width;
     this.swingAngle = swingAngle;
-    this.attackDuration = attackDuration; // in milliseconds
-    this.cooldown = cooldown; // in milliseconds
+    this.attackDuration = attackDuration;
+    this.cooldown = cooldown;
     this.color = color;
   }
 }

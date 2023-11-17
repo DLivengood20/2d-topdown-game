7fef780958b473e74657df00289561d5355a9c8d
David Livengood <dlivengood@worcester.edu>
2023-11-17 16:05:56 -0500

    Cleaned up collision system

--- src/systems/collision.system.ts
+++ src/systems/collision.system.ts
@@ -9,43 +9,6 @@ import { System } from './system';
  */
 export class CollisionSystem implements System {
   /**
-   * Checks for collisions with a specific entity among a set of collidables.
-   * @param {Entity} entity - The entity for which collisions are checked.
-   * @param {Entity[]} collidables - An array of entities that could potentially collide with the specified entity.
-   * @returns {Entity[]} An array of entities collided with the specified entity.
-   */
-  private checkCollisionWith(
-    entity: Entity,
-    collidables: Array<Entity>
-  ): Entity[] {
-    const collidedWith = [];
-    const entityComponent = entity.getComponent(PhysicalComponent);
-
-    for (const collidable of collidables) {
-      const collidableComponent = collidable.getComponent(PhysicalComponent);
-      if (collidableComponent && entityComponent) {
-        const { x, y, width, height } = collidableComponent;
-        if (
-          x + width / 2 > entityComponent.x - entityComponent.width / 2 &&
-          x - width / 2 < entityComponent.x + entityComponent.width / 2 &&
-          y + height / 2 > entityComponent.y - entityComponent.height / 2 &&
-          y - height / 2 < entityComponent.y + entityComponent.height / 2
-        ) {
-          collidedWith.push(collidable);
-
-          // Inflict damage based on collision settings
-          const entityStatus = entity.getComponent(StatusComponent);
-          const collidedStatus = collidable.getComponent(StatusComponent);
-          if (entityStatus && collidedStatus) {
-            entityStatus.damageTaken += collidedStatus.collisionDamage;
-          }
-        }
-      }
-    }
-    return collidedWith;
-  }
-
-  /**
    * Updates the collision status for each entity in the game.
    * @param {Entity[]} entities - An array of entities in the game.
    */
@@ -64,5 +27,57 @@ export class CollisionSystem implements System {
         );
       }
     }
+  }
+
+  /**
+   * Checks for collisions with a specific entity among a set of collidables.
+   * @param {Entity} entity - The entity for which collisions are checked.
+   * @param {Entity[]} collidables - An array of entities that could potentially collide with the specified entity.
+   * @returns {Entity[]} An array of entities collided with the specified entity.
+   */
+  private checkCollisionWith(
+    entity: Entity,
+    collidables: Array<Entity>
+  ): Entity[] {
+    const collidedWith: Entity[] = [];
+    const entityComponent = entity.getComponent(PhysicalComponent);
+
+    for (const collidable of collidables) {
+      const collidableComponent = collidable.getComponent(PhysicalComponent);
+      if (
+        collidableComponent &&
+        entityComponent &&
+        this.determineCollision(entityComponent, collidableComponent)
+      ) {
+        collidedWith.push(collidable);
+
+        // Inflict damage based on collision settings
+        const entityStatus = entity.getComponent(StatusComponent);
+        const collidedStatus = collidable.getComponent(StatusComponent);
+        if (entityStatus && collidedStatus) {
+          entityStatus.damageTaken += collidedStatus.collisionDamage;
+        }
+      }
+    }
+    return collidedWith;
+  }
+
+  /**
+   * Determines whether two physical components are colliding.
+   * @param {PhysicalComponent} physicalComponent_1 - The first physical component.
+   * @param {PhysicalComponent} physicalComponent_2 - The second physical component.
+   * @returns {boolean} Returns true if the two components are colliding, otherwise false.
+   */
+  private determineCollision(
+    physicalComponent_1: PhysicalComponent,
+    physicalComponent_2: PhysicalComponent
+  ): boolean {
+    const { x, y, width, height } = physicalComponent_1;
+    return (
+      x + width / 2 > physicalComponent_2.x - physicalComponent_2.width / 2 &&
+      x - width / 2 < physicalComponent_2.x + physicalComponent_2.width / 2 &&
+      y + height / 2 > physicalComponent_2.y - physicalComponent_2.height / 2 &&
+      y - height / 2 < physicalComponent_2.y + physicalComponent_2.height / 2
+    );
   }
 }

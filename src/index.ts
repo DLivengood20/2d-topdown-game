9fb4434b35c1384f90b19edfaaffbd760ed9468e
David Livengood <dlivengood@worcester.edu>
2023-11-18 11:56:09 -0500

    Commented index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-// Import necessary libraries
+/**
+ * Import necessary libraries.
+ * @module
+ */
 import { Game } from './game';
 
-// Create an instance of the game
+/**
+ * Create an instance of the game.
+ * @const {Game | null}
+ */
 const game = Game.createGame();
 
+/**
+ * Start the game loop if the game instance is not null.
+ * Log an error message if the game is null.
+ */
 if (game) {
-  // Start the game loop
+  /**
+   * Function to start the game loop.
+   * @function
+   */
   game.startGameLoop();
 } else {
+  /**
+   * Log an error message if the game instance is null.
+   * @type {string}
+   */
   console.error('Game is null');
 }
